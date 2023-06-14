8ae63c789e848c9dc2381af1ed846b71805d499d
dCRYPT <39372980+adeolu98@users.noreply.github.com>
2023-06-14 17:45:15 +0100

    finish level

--- src/Attacker.sol
+++ src/Attacker.sol
@@ -2,24 +2,6 @@
 pragma solidity ^0.8.0;
 import "./Contract.sol";
 
-contract Attacker is Building {
-    uint counter;
-    Elevator elevatorContract;
+contract Attacker  {
 
-    constructor(address _elevatorContract) {
-        elevatorContract = Elevator(_elevatorContract);
-    }
-
-    function isLastFloor(uint _floor) external override returns (bool) {
-        if (counter == 0) {
-            counter++;
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function attack() public {
-        elevatorContract.goTo(1);
-    }
 }
--- src/Contract.sol
+++ src/Contract.sol
@@ -1,26 +1,52 @@
-// This elevator won't let you reach the top of your building. Right?
+// The creator of this contract was careful enough to protect the sensitive areas of its storage.
+
+// Unlock this contract to beat the level.
 
 // Things that might help:
-// Sometimes solidity is not good at keeping promises.
-// This Elevator expects to be used from a Building.
+
+// Understanding how storage works
+// Understanding how parameter parsing works
+// Understanding how casting works
+// Tips:
+
+// Remember that metamask is just a commodity. Use another tool if it is presenting problems. Advanced gameplay could involve using remix, or your own web3 provider.
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface Building {
-  function isLastFloor(uint) external returns (bool);
-}
+contract Privacy {
 
+//storage slot 0
+  bool public locked = true;
 
-contract Elevator {
-  bool public top;
-  uint public floor;
+  //storage slot 1
+  uint256 public ID = block.timestamp;
 
-  function goTo(uint _floor) public {
-    Building building = Building(msg.sender);
+  //storage slot 2
+  uint8 private flattening = 10;
+  uint8 private denomination = 255;
+  uint16 private awkwardness = uint16(block.timestamp);
 
-    if (! building.isLastFloor(_floor)) {
-      floor = _floor;
-      top = building.isLastFloor(floor);
-    }
+  //takes storage slot 3 to 5
+  bytes32[3] private data;
+
+  constructor(bytes32[3] memory _data) {
+    data = _data;
   }
+  
+  function unlock(bytes16 _key) public {
+    //valid _key is in storage slot 5
+    //read it via await web3.eth.getStorageAt('0x42d456038445cA6bdA8E1C8f95E3212FA81E5763', 5)
+    require(_key == bytes16(data[2]));
+    locked = false;
+  }
+
+  /*
+    A bunch of super advanced solidity algorithms...
+
+      ,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`
+      .,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,
+      *.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^         ,---/V\
+      `*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.    ~|__(o.o)
+      ^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'^`*.,*'  UU  UU
+  */
 }
